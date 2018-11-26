386070ee886e93322f12f1a167efa7df17756662
Marcos Rativa <marcosr@imaginacolombia.com>
2018-11-26 11:36:24 -0500

    preparing Version 3.5.2

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -5,21 +5,25 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['prefix' => '/slide'], function (Router $router) {
-    $router->post('/update', [
-        'as' => 'api.slide.update',
-        'uses' => 'SlideController@update',
-        'middleware' => 'token-can:slider.slides.update',
-    ]);
-    $router->post('/delete', [
-        'as' => 'api.slide.delete',
-        'uses' => 'SlideController@delete',
-        'middleware' => 'token-can:slider.slides.destroy'
-    ]);
-});
+  $router->get('/', [
+    'as' => 'api.slide.index',
+    'uses' => 'SlideApiController@index'
+  ]);
 
-$router->group(['prefix' => '/slider'], function (Router $router) {
   $router->get('/{id}', [
     'as' => 'api.slide.show',
-    'uses' => 'SliderApiController@show'
+    'uses' => 'SlideApiController@show'
+  ]);
+
+  $router->post('/update', [
+    'as' => 'api.slide.update',
+    'uses' => 'SlideController@update',
+    'middleware' => 'token-can:slider.slides.update',
+  ]);
+
+  $router->post('/delete', [
+    'as' => 'api.slide.delete',
+    'uses' => 'SlideController@delete',
+    'middleware' => 'token-can:slider.slides.destroy'
   ]);
 });
