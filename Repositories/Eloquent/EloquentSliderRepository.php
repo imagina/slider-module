7de2bbf0aae82a27b12f9e6068fa5a8ee9d30028
Juan Escalante <jcec007@gmail.com>
2020-12-17 16:33:08 -0500

    +Update: EloquentSliderRepository | include relations slides and slides.files

--- Repositories/Eloquent/EloquentSliderRepository.php
+++ Repositories/Eloquent/EloquentSliderRepository.php
@@ -8,47 +8,47 @@ use Modules\Slider\Repositories\SliderRepository;
 
 class EloquentSliderRepository extends EloquentBaseRepository implements SliderRepository
 {
-    public function create($data)
-    {
-        $slider = $this->model->create($data);
-
-        return $slider;
-    }
-
-    public function update($slider, $data)
-    {
-        $slider->update($data);
-
-        return $slider;
-    }
-
-    /**
-     * Count all records
-     * @return int
-     */
-    public function countAll()
-    {
-        return $this->model->count();
-    }
-
-    /**
-     * Get all available sliders
-     * @return object
-     */
-    public function allOnline()
-    {
-        return $this->model->where('active', '=', true)
-            ->orderBy('created_at', 'DESC')
-            ->get();
-    }
-
-
-    /**
-     * @param string $systemName
-     * @return Slider
-     */
-    public function findBySystemName($systemName)
-    {
-        return $this->model->where('system_name', '=', $systemName)->first();
-    }
+  public function create($data)
+  {
+    $slider = $this->model->create($data);
+    
+    return $slider;
+  }
+  
+  public function update($slider, $data)
+  {
+    $slider->update($data);
+    
+    return $slider;
+  }
+  
+  /**
+   * Count all records
+   * @return int
+   */
+  public function countAll()
+  {
+    return $this->model->count();
+  }
+  
+  /**
+   * Get all available sliders
+   * @return object
+   */
+  public function allOnline()
+  {
+    return $this->model->where('active', '=', true)
+      ->orderBy('created_at', 'DESC')
+      ->get();
+  }
+  
+  
+  /**
+   * @param string $systemName
+   * @return Slider
+   */
+  public function findBySystemName($systemName)
+  {
+    return $this->model->where('system_name', '=', $systemName)->with(["slides", "slides.files"])->first();
+  }
 }
