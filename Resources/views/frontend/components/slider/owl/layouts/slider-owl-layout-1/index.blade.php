7e8f241b8332f2b86395cc09f9b94aa08b7753ae
Juan Escalante <jcec007@gmail.com>
2021-03-20 11:30:09 -0500

    +Update: addded iframe for videos in the Slider blade component

--- Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-1/index.blade.php
+++ Resources/views/frontend/components/slider/owl/layouts/slider-owl-layout-1/index.blade.php
@@ -1,43 +1,61 @@
 <div id="{{ $slider->system_name }}"
-     class="owl-carousel slider-component owl-theme owl-slider-layout-1{{ $dots ? ' owl-with-dots carousel-indicators-position-'.$dotsPosition.' carousel-indicators-style-'. $dotsStyle: '' }}" style="max-height: {{ $height }}">
+     class="owl-carousel slider-component owl-theme owl-slider-layout-1{{ $dots ? ' owl-with-dots carousel-indicators-position-'.$dotsPosition.' carousel-indicators-style-'. $dotsStyle: '' }}"
+     style="max-height: {{ $height }}">
     @foreach($slider->slides as $index => $slide)
-        <div class="item h-100">
+
+        @switch($slide->type)
+
+
+            @case("auto")
+            @case("image")
+
             <x-media::single-image :alt="$slide->title ?? Setting::get('core::site-name')"
                                    :title="$slide->title ?? Setting::get('core::site-name')"
                                    :url="$slide->uri ?? $slide->url ?? null" :isMedia="true"
                                    imgClasses="d-block h-100 slider-img__{{$imgObjectFit}}"
                                    width="100%"
-                                   :mediaFiles="$slide->mediaFiles()" zone="slideimage" />
+                                   :mediaFiles="$slide->mediaFiles()" zone="slideimage"/>
             @if(!empty($slide->title) || !empty($slide->caption) || !empty($slide->custom_html))
-            <div class="carousel-caption px-o pb-0 d-none d-md-block h-100">
-                <div class="container h-100">
-                    <div class="row h-100 justify-content-center">
-                        <div class="col-10 text-center">
+                <div class="carousel-caption px-o pb-0 d-none d-md-block h-100">
+                    <div class="container h-100">
+                        <div class="row h-100 justify-content-center">
+                            <div class="col-10 text-center">
 
-                            @if(!empty($slide->title))
-                                <h1 class="title1 mb-2 h1"><b>{{$slide->title}}</b></h1>
-                            @endif
+                                @if(!empty($slide->title))
+                                    <h1 class="title1 mb-2 h1"><b>{{$slide->title}}</b></h1>
+                                @endif
 
-                            @if(!empty($slide->custom_html))
-                                <div class="custom-html d-none d-md-block">
-                                    {!! $slide->custom_html !!}
+                                @if(!empty($slide->custom_html))
+                                    <div class="custom-html d-none d-md-block">
+                                        {!! $slide->custom_html !!}
+                                    </div>
+                                @endif
+
+                                <div class="d-block">
+                                    <a class="btn btn-primary"
+                                       href="{{ $slide->url ?? $slide->uri }}">{{ $slide->caption ?? trans('isite::common.menu.viewMore') }}</a>
                                 </div>
-                            @endif
 
-                            <div class="d-block">
-                                <a class="btn btn-primary" href="{{ $slide->url ?? $slide->uri }}">{{ $slide->caption ?? trans('isite::common.menu.viewMore') }}</a>
                             </div>
-
                         </div>
                     </div>
                 </div>
-            </div>
+
             @endif
-        </div>
+            @break
+
+            @case("video")
+            <div class="item h-100">
+                <iframe class="full-height" width="100%" height="{{$height}}" src="{{ $slide->getLinkUrl() }}"
+                        frameborder="0" allowfullscreen></iframe>
+            </div>
+            @break
+
+        @endswitch
     @endforeach
 </div>
 <script>
-  $(function(){
+  $(function () {
     $('#{{ $slider->system_name }}').owlCarousel({
       items: 1,
       dots: {!! $dots ? 'true' : 'false' !!},
@@ -47,7 +65,7 @@
       nav: {!! $nav ? 'true' : 'false' !!},
       autoplay: {!! $autoplay ? 'true' : 'false' !!},
       autoplayHoverPause: {!! $autoplayHoverPause ? 'true' : 'false' !!},
-      {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
+        {!! !empty($navText) ? 'navText: '.$navText."," : "" !!}
     });
   });
 </script>
